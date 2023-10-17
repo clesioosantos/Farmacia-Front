a5e27e646ab2ab0be8435071ad1df52c7341eabb
Clésio Santos <clesio0420@gmail.com>
2023-10-17 14:45:10 -0300

    edit 2

--- farmacia/src/components/navbar/Navbar.tsx
+++ farmacia/src/components/navbar/Navbar.tsx
@@ -1,29 +1,41 @@
-import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import user from "../../assets/user.png";
+import car from "../../assets/carrinho.png";
 
 function Navbar() {
   return (
-    <>
-      <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
-        <div className="container flex justify-between text-lg">
-          <div className="text-2xl font-bold uppercase">Blog Pessoal</div>
+    <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+      <div className="container flex items-center justify-between text-lg">
+        <div className="max-w-xs max-h-20">
+          {" "}
+          <img src={logo} alt="" className="" />{" "}
+        </div>
 
-          <div className="flex gap-4">
-            <Link to="/login" className="hover:underline">
-              Login
-            </Link>
-            <Link to="/home" className="hover:underline">
-              Home
-            </Link>
-            <div className="hover:underline">Postagens</div>
-            <div className="hover:underline">Temas</div>
-            <div className="hover:underline">Cadastrar tema</div>
-            <div className="hover:underline">Perfil</div>
-            <div className="hover:underline">Sair</div>
+        <div className="flex">
+          <label className="relative block w-96">
+            <input
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Pesquisar"
+              type="text"
+              name="search"
+            />
+          </label>
+        </div>
+
+        <div className="flex  gap-4">
+          <div className="hover:underline">Categorias</div>
+          <div className="hover:underline">Cadastrar categoria</div>
+          <div className="hover:underline">
+            <img src={user} alt="" className="w-3/4" />
+          </div>
+          <div className="hover:underline">
+            <img src={car} alt="" className="w-3/4" />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
